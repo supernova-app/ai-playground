82e12cab0d4d0d82cf9799922c16b248b29d4391
Vaibhav Acharya <vaibhav@gosupernova.live>
2025-01-08 11:17:47 +0530

    refactor ai config

--- app/lib/ai.ts
+++ app/lib/ai.ts
@@ -2,6 +2,7 @@ import {
   experimental_createProviderRegistry as createProviderRegistry,
   type Experimental_LanguageModelV1Middleware as LanguageModelV1Middleware,
   type LanguageModelV1StreamPart,
+  type Provider,
 } from "ai";
 
 import type { SUPPORTED_PROVIDER_KEY } from "~/config/ai";
@@ -14,62 +15,60 @@ import { createVertex } from "@ai-sdk/google-vertex";
 
 import { db, schema } from "./db";
 
-export const providerRegistry = createProviderRegistry({
-  ...(process.env.AI_OPENAI_API_KEY
-    ? {
-        openai: createOpenAI({
-          baseURL: process.env.AI_OPENAI_API_URL,
-          apiKey: process.env.AI_OPENAI_API_KEY,
-          organization: process.env.AI_OPENAI_ORGANIZATION,
-          project: process.env.AI_OPENAI_PROJECT,
-          compatibility: "strict",
-        }),
-      }
-    : {}),
-  ...(process.env.AI_ANTHROPIC_API_KEY
-    ? {
-        anthropic: createAnthropic({
-          baseURL: process.env.AI_ANTHROPIC_API_URL,
-          apiKey: process.env.AI_ANTHROPIC_API_KEY,
-        }),
-      }
-    : {}),
-  ...(process.env.AI_AZURE_API_KEY
-    ? {
-        azure: createAzure({
-          resourceName: process.env.AI_AZURE_RESOURCE_NAME,
-          apiKey: process.env.AI_AZURE_API_KEY,
-          apiVersion: process.env.AI_AZURE_API_VERSION,
-          baseURL: process.env.AI_AZURE_API_URL,
-        }),
-      }
-    : {}),
-  ...(process.env.GOOGLE_GENERATIVE_AI_API_KEY
-    ? {
-        google: createGoogleGenerativeAI({
-          baseURL: process.env.AI_GOOGLE_GENERATIVE_AI_API_URL,
-          apiKey: process.env.AI_GOOGLE_GENERATIVE_AI_API_KEY,
-        }),
-      }
-    : {}),
-  ...(process.env.AI_VERTEX_GOOGLE_PROJECT &&
+let config: Partial<Record<SUPPORTED_PROVIDER_KEY, Provider>> = {};
+
+if (process.env.AI_OPENAI_API_KEY) {
+  config.openai = createOpenAI({
+    baseURL: process.env.AI_OPENAI_API_URL,
+    apiKey: process.env.AI_OPENAI_API_KEY,
+    organization: process.env.AI_OPENAI_ORGANIZATION,
+    project: process.env.AI_OPENAI_PROJECT,
+    compatibility: "strict",
+  });
+}
+
+if (process.env.AI_ANTHROPIC_API_KEY) {
+  config.anthropic = createAnthropic({
+    baseURL: process.env.AI_ANTHROPIC_API_URL,
+    apiKey: process.env.AI_ANTHROPIC_API_KEY,
+  });
+}
+
+if (process.env.AI_AZURE_API_KEY) {
+  config.azure = createAzure({
+    resourceName: process.env.AI_AZURE_RESOURCE_NAME,
+    apiKey: process.env.AI_AZURE_API_KEY,
+    apiVersion: process.env.AI_AZURE_API_VERSION,
+    baseURL: process.env.AI_AZURE_API_URL,
+  });
+}
+
+if (process.env.GOOGLE_GENERATIVE_AI_API_KEY) {
+  config.google = createGoogleGenerativeAI({
+    baseURL: process.env.AI_GOOGLE_GENERATIVE_AI_API_URL,
+    apiKey: process.env.AI_GOOGLE_GENERATIVE_AI_API_KEY,
+  });
+}
+
+if (
+  process.env.AI_VERTEX_GOOGLE_PROJECT &&
   process.env.AI_VERTEX_GOOGLE_CLIENT_EMAIL &&
   process.env.AI_VERTEX_GOOGLE_PRIVATE_KEY
-    ? {
-        vertex: createVertex({
-          project: process.env.AI_VERTEX_GOOGLE_PROJECT,
-          location: process.env.AI_VERTEX_GOOGLE_LOCATION,
-          baseURL: process.env.AI_VERTEX_GOOGLE_API_URL,
-          googleAuthOptions: {
-            credentials: {
-              client_email: process.env.AI_VERTEX_GOOGLE_CLIENT_EMAIL,
-              private_key: process.env.AI_VERTEX_GOOGLE_PRIVATE_KEY,
-            },
-          },
-        }),
-      }
-    : {}),
-} satisfies Partial<Record<SUPPORTED_PROVIDER_KEY, unknown>>);
+) {
+  config.vertex = createVertex({
+    project: process.env.AI_VERTEX_GOOGLE_PROJECT,
+    location: process.env.AI_VERTEX_GOOGLE_LOCATION,
+    baseURL: process.env.AI_VERTEX_GOOGLE_API_URL,
+    googleAuthOptions: {
+      credentials: {
+        client_email: process.env.AI_VERTEX_GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.AI_VERTEX_GOOGLE_PRIVATE_KEY,
+      },
+    },
+  });
+}
+
+export const providerRegistry = createProviderRegistry(config);
 
 export const logMiddleware: LanguageModelV1Middleware = {
   wrapGenerate: async ({ model, params, doGenerate }) => {
